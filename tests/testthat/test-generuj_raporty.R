6684e38c537a626cc90d907f6f2be242c0920b37
tzoltak <tomek@zozlak.org>
2019-02-02 17:37:26 +0100

    i jeszcze boje z Travisem

--- tests/testthat/test-generuj_raporty.R
+++ tests/testthat/test-generuj_raporty.R
@@ -17,18 +17,18 @@ stanFolderu = list.files()
 #                nazwyRaportowPDF)
 # })
 #
-# test_that("Raporty HTML na podstawie szablonu 'raport_szkoly.RMD'", {
-#   nazwyRaportowPDF =
-#     generuj_raporty('raport_szkoly.Rmd',
-#                     wskaznikiSzk,
-#                     wskaznikiTypSzk,
-#                     SZK_kod,
-#                     parametry = list(typDokumentu = "html",
-#                                      progLiczebnosci = 10,
-#                                      rocznik = 2017,
-#                                      wyrownanieTabWykr = "center"))
-#   expect_equal(paste0(wskaznikiSzk$SZK_kod, ".html"),
-#                nazwyRaportowPDF)
-# })
+test_that("Raporty HTML na podstawie szablonu 'raport_szkoly.RMD'", {
+  nazwyRaportowPDF =
+    generuj_raporty('raport_szkoly.Rmd',
+                    wskaznikiSzk,
+                    wskaznikiTypSzk,
+                    SZK_kod,
+                    parametry = list(typDokumentu = "html",
+                                     progLiczebnosci = 10,
+                                     rocznik = 2017,
+                                     wyrownanieTabWykr = "center"))
+  expect_equal(paste0(wskaznikiSzk$SZK_kod, ".html"),
+               nazwyRaportowPDF)
+})
 
 unlink(setdiff(list.files(), stanFolderu), recursive = TRUE)
